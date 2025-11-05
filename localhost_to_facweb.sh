0c9ee40e578e19ce512796eec7f25a208cedb43a
Zachary Kurmas <kurmasz@gvsu.edu>
2025-11-05 11:19:07 -0500

    Add 452 lab 11

--- localhost_to_facweb.sh
+++ localhost_to_facweb.sh
@@ -1,1 +1,2 @@
-find . \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/http:\/\/localhost:8000/https:\/\/faculty.computing.gvsu.edu\/kurmasz/g'
+# There was a reason this didn't intially have -name '*.html'; but, I don't remember what it is.
+find . \( -type d -name .git -prune \) -o -type f -name '*.html' -print0 | xargs -0 sed -i 's/http:\/\/localhost:8000/https:\/\/faculty.computing.gvsu.edu\/kurmasz/g'
